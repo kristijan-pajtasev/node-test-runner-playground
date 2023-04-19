5af0b42db7f566f467070ffb85b4ec73c87d70e7
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-04-19 23:31:45 +0100

    first tests

--- helpers.test.mjs
+++ helpers.test.mjs
@@ -1,1 +1,12 @@
 import {addNumbers} from "./helpers.mjs";
+import test from 'node:test';
+import assert from 'node:assert';
+
+test("adds two numbers", function() {
+  assert.strictEqual(addNumbers(1, 1), 2);
+  assert.strictEqual(addNumbers(1, 2), 3);
+})
+
+test("adds two numbers", function() {
+  assert.notEqual(addNumbers(1, 1), 3);
+})
