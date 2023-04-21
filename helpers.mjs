5fc6bbaa5eb728e8ba7358a968efb5d6370ec01f
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-04-21 22:04:58 +0100

    new tests added

--- helpers.mjs
+++ helpers.mjs
@@ -1,4 +1,6 @@
 function addNumbers(first, second) {
+  if(typeof first !== "number") throw new Error("Invalid first parameter type");
+  if(typeof second !== "number") throw new Error("Invalid second parameter type");
   return first + second;
 }
 
--- helpers.test.mjs
+++ helpers.test.mjs
@@ -1,12 +1,24 @@
 import {addNumbers} from "./helpers.mjs";
-import test from 'node:test';
+import {test, describe} from 'node:test';
 import assert from 'node:assert';
 
-test("adds two numbers", function() {
-  assert.strictEqual(addNumbers(1, 1), 2);
-  assert.strictEqual(addNumbers(1, 2), 3);
-})
 
-test("adds two numbers", function() {
-  assert.notEqual(addNumbers(1, 1), 3);
+describe("helpers function addNumbers", function () {
+  test("adds two numbers", function () {
+    assert.strictEqual(addNumbers(1, 1), 2);
+    assert.strictEqual(addNumbers(1, 2), 3);
+    assert.strictEqual(addNumbers(1, 3), 4);
+  })
+
+  test("throws error when first parameter invalid", function () {
+    assert.throws(() => {
+      addNumbers("invalid value", 2)
+    }, {message: "Invalid first parameter type"});
+  })
+
+  test("throws error when second parameter invalid", function () {
+    assert.throws(() => {
+      addNumbers(1, "2")
+    }, {message: "Invalid second parameter type"});
+  })
 })
